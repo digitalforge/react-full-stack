c9b9ad1e2caaba271c761092bbb6f8a70b900edd
digitalforge <digitalforge@outlook.com>
2025-12-10 21:41:07 -0500

    added appwrite database

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -2,7 +2,10 @@ import React, { use } from 'react'
 import heroImg from './assets/images/hero-img.png'
 import Search from './components/Search'
 import Spinner from './components/Spinner'
+import MovieCard from './components/MovieCard'
+import { useDebounce } from 'react-use'
 import { useState, useEffect } from 'react'
+import { updateSearchCount } from '../appwrite'
 
 const API_BASE_URL = 'https://api.themoviedb.org/3'
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY // Accessing the API key from environment variables
@@ -16,15 +19,20 @@ const API_OPTIONS = {
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('')
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('')
   const [errorMessage, setErrorMessage] = useState('')
   const [movieList, setMovieList] = useState([])
   const [isLoading, setIsLoading] = useState(false)
 
-  const fetchMovies = async () => {
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm])
+
+  const fetchMovies = async (query = '') => {
     setIsLoading(true)
     setErrorMessage('') // Clear any previous error messages
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}` // encodeURIComponent is used to make the query URL safe by encoding special characters. Example: spaces become %20
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
       const response = await fetch(endpoint, API_OPTIONS)
       if (!response.ok) {
         //this works like a catch for fetch errors so why do we need a catch block? Well, fetch only throws an error for network issues, not for HTTP errors like 404 or 500. So we check response.ok to handle those cases.
@@ -42,6 +50,11 @@ const App = () => {
 
       setMovieList(data.results)
       setIsLoading(false)
+
+      if (query && data.results.length > 0) {
+        // update search count in Appwrite only if there's a valid search term and results
+        await updateSearchCount(query, data.results[0])
+      }
     } catch (error) {
       setErrorMessage('Failed to fetch movies. Please try again later.')
     } finally {
@@ -50,8 +63,8 @@ const App = () => {
   }
 
   useEffect(() => {
-    fetchMovies()
-  }, []) // What is useEffect? It runs side effects. Side effects are operations that affect something outside the component, like fetching data, setting up subscriptions, or manually changing the DOM.
+    fetchMovies(debouncedSearchTerm)
+  }, [debouncedSearchTerm]) // What is useEffect? It runs side effects. Side effects are operations that affect something outside the component, like fetching data, setting up subscriptions, or manually changing the DOM.
 
   return (
     <div className='pattern'>
@@ -76,9 +89,7 @@ const App = () => {
                 (
                   movie // when you don't use curly braces, you don't need a return statement because it's an implicit return
                 ) => (
-                  <p key={movie.id} className='text-white'>
-                    {movie.title}
-                  </p>
+                  <MovieCard key={movie.id} movie={movie} />
                 )
               )}
             </ul>
